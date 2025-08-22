60ff9316a1e2751321b2ea64a8f7c68cd2aeb114
Giovanna <giovannapiccinato08@gmail.com>
2025-08-22 01:45:54 -0300

    feat: arrumando

--- Primeiro_Projeto/src/app/app.ts
+++ Primeiro_Projeto/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Primeiro_Projeto');
+  protected readonly title = signal('primeiro_projeto');
 }
